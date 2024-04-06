865f9e0274648a7da332efe95ab5767a65fa6de7
Michael <mpierson00@gmail.com>
2024-04-05 20:08:40 -0400

    added function to initailize app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -58,8 +59,14 @@ function writeToFile(fileName, data) {
 );
 }
 
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+    });
+}
 
 // Function call to initialize app
 init();
